104074afd9b7078fce0f66a93cf42006a05a9c82
Kate <katok0709@gmail.com>
2022-05-10 15:25:04 +0300

    create simple Photos page

--- src/screens/Photos.js
+++ src/screens/Photos.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {
-    Text,
     View,
     Button,
+    Image,
+    StyleSheet
 } from 'react-native';
 
 function Photos({ route, navigation }) {
@@ -12,14 +13,61 @@ function Photos({ route, navigation }) {
         setTextVisible,
     } = route.params;
 
+    const styles = StyleSheet.create({
+        container: {
+            marginLeft: 15,
+            flexWrap: 'wrap',
+            paddingTop: 50,
+            maxHeight: '70%',
+            alignContent: 'center',
+            justifyContent: 'center'
+        },
+        image: {
+            width: 150,
+            height: 150,
+            marginBottom: 10,
+            marginRight: 10
+        },
+        logo: {
+            width: 66,
+            height: 58,
+        },
+    });
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Photos</Text>
-            <Button title="go Home" onPress={() => {
-                navigation.navigate('Home')
-                setTextVisible(false)
-                setImageVisible(false)
-            }} />
+        <View style={styles.container}>
+
+            <Image
+                style={styles.image}
+                source={require('../assets/images/kates_face2.png')}
+            />
+            <Image
+                style={styles.image}
+                source={require('../assets/images/kates_face3.png')}
+            />
+            <Image
+                style={styles.image}
+                source={require('../assets/images/kates_face4.png')}
+            />
+            <Image
+                style={styles.image}
+                source={require('../assets/images/kates_face5.png')}
+            />
+            <View
+                style={{
+                    backgroundColor: 'pink',
+                    borderRadius: 30
+                }}
+            >
+                <Button
+                    title="go Home"
+                    onPress={() => {
+                        navigation.navigate('Home')
+                        setTextVisible(false)
+                        setImageVisible(false)
+                    }}
+                />
+            </View>
         </View>
     )
 }
